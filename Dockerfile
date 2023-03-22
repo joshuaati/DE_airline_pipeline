d7723794eea9cad8fb629c48f15c6fcac3330c9b
joshuaati <joshuaati97@gmail.com>
2023-03-22 15:51:39 +0000

    setting up

---
+++ Dockerfile
@@ -0,0 +1,42 @@
+FROM python:3.10-slim-buster
+
+# Install Jupyter
+RUN pip install jupyter
+
+# Install Prefect
+RUN pip install prefect
+
+# Install Git
+RUN apt-get update && \
+    apt-get install -y git
+
+
+# Install PySpark
+RUN apt-get update && \
+    apt-get install -y openjdk-11-jdk-headless && \
+    pip install pyspark
+
+# Install dbt-bigquery
+RUN pip install dbt-bigquery
+
+# Install Piperider
+RUN pip install piperider
+
+# Install Google Cloud 
+RUN pip install google-cloud-storage
+RUN pip install google-cloud-bigquery
+
+
+# Set environment variables for PySpark
+ENV PYSPARK_PYTHON=python3
+ENV PYSPARK_DRIVER_PYTHON=jupyter
+ENV PYSPARK_DRIVER_PYTHON_OPTS='lab --ip=0.0.0.0 --port=8888 --no-browser'
+
+# Expose port for Jupyter
+EXPOSE 8888
+
+# Set working directory
+WORKDIR /app
+
+# Start PySpark
+CMD ["pyspark"]
---
+++ main/etl_web_gcp.py
@@ -0,0 +1,88 @@
+import os
+import zipfile
+import requests
+from typing import Tuple
+from google.cloud import storage
+from typing import List, Tuple
+
+
+def download_data(url: str, output_dir: str) -> str:
+    """
+    Downloads a zip file from the specified url and saves it to the specified
+    output directory.
+
+    Args:
+        url: The url of the zip file to download.
+        output_dir: The directory to save the downloaded file in.
+
+    Returns:
+        The path to the downloaded file.
+    """
+    os.makedirs(output_dir, exist_ok=True)
+    file_path = os.path.join(output_dir, os.path.basename(url))
+
+    response = requests.get(url, stream=True)
+    with open(file_path, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+
+    return file_path
+
+
+def extract_data(file_path: str, output_dir: str) -> Tuple[str, str]:
+    """
+    Extracts a zip file to the specified output directory.
+
+    Args:
+        file_path: The path to the zip file to extract.
+        output_dir: The directory to extract the file to.
+
+    Returns:
+        A tuple containing the paths to the extracted files.
+    """
+    os.makedirs(output_dir, exist_ok=True)
+    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        zip_ref.extractall(output_dir)
+
+    extracted_files = []
+    for root, _, files in os.walk(output_dir):
+        for file in files:
+            extracted_files.append(os.path.join(root, file))
+
+    return tuple(extracted_files)
+
+
+def write_to_gcs(bucket_name: str, output_dir: str, extracted_files: Tuple[str, ...]):
+    """
+    Uploads the contents of each file in the extracted_files list to a Google Cloud Storage bucket.
+
+    Args:
+        bucket_name: The name of the Google Cloud Storage bucket to upload to.
+        output_dir: The directory where the extracted files are stored.
+        extracted_files: A tuple containing the paths to the extracted files.
+
+    Returns:
+        None
+    """
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+
+    for file_path in extracted_files:
+        with open(file_path, 'rb') as f:
+            content = f.read()
+
+        filename = os.path.join(output_dir, os.path.basename(file_path))
+
+        blob = bucket.blob(filename)
+        blob.upload_from_string(content)
+
+
+url = 'https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/HG7NV7/GIZV7R'
+output_dir = '../data'
+bucket_name = 'my_bucket_name'
+
+file_path = download_data(url, output_dir)
+extracted_files = extract_data(file_path, output_dir)
+print(extracted_files)
+#write_to_gcs(bucket_name, output_dir, extracted_files)
