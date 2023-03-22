8c3acfdc2f776c7b9fbe343cd1c3974cf2e4b1b2
joshuaati <joshuaati97@gmail.com>
2023-03-22 22:28:43 +0000

    prefect deployment

--- main/etl_web_gcp.py
+++ main/etl_web_gcp.py
@@ -3,18 +3,19 @@ import zipfile
 import requests
 from typing import Tuple
 from google.cloud import storage
+from datetime import timedelta
 from typing import List, Tuple
 from prefect import flow, task
-from prefect.tasks import task_input_hash
+from prefect.tasks import task_input_hash, exponential_backoff
 
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS']='/Users/x/OneDrive/Documents/Python/Jan_2023/DE_Zoomcamp/week_4_analytics_engineering/.google/credentials/google_credentials.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS']='/Users/x/.google/credentials/airlinepipeline.json'
 
 # prevent timeout due to low network
 storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024* 1024  # 5 MB
 storage.blob._MAX_MULTIPART_SIZE = 5 * 1024* 1024  # 5 MB
 
-@task(retries=3, )
+@task(name='download_data', retries=3, retry_delay_seconds=exponential_backoff(backoff_factor=10), cache_key_fn=task_input_hash, cache_expiration=timedelta(hours=2))
 def download_data(url: str, output_dir: str) -> str:
     """
     Downloads a zip file from the specified url and saves it to the specified
@@ -38,7 +39,7 @@ def download_data(url: str, output_dir: str) -> str:
 
     return file_path
 
-
+@task(name='extract_data', retries=3, retry_delay_seconds=exponential_backoff(backoff_factor=5))
 def extract_data(file_path: str, output_dir: str) -> Tuple[str, str]:
     """
     Extracts a zip file to the specified output directory.
@@ -61,7 +62,7 @@ def extract_data(file_path: str, output_dir: str) -> Tuple[str, str]:
 
     return tuple(extracted_files)
 
-
+@task(name='write_data', retries=3, retry_delay_seconds=exponential_backoff(backoff_factor=10))
 def write_to_gcs(bucket_name: str, output_dir: str, extracted_files: Tuple[str, ...]):
     """
     Uploads the contents of each file in the extracted_files list to a Google Cloud Storage bucket.
@@ -86,12 +87,16 @@ def write_to_gcs(bucket_name: str, output_dir: str, extracted_files: Tuple[str, 
         blob = bucket.blob(filename)
         blob.upload_from_string(content)
 
+@flow()
+def main_etl():
+    url = 'https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/HG7NV7/GIZV7R'
+    output_dir = '../data'
+    bucket_name = 'airline-buckets'
 
-url = 'https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/HG7NV7/GIZV7R'
-output_dir = '../data'
-bucket_name = 'my_bucket_name'
+    file_path = download_data(url, output_dir)
+    extracted_files = extract_data(file_path, output_dir)
+    print(extracted_files)
+    write_to_gcs(bucket_name, output_dir, extracted_files)
 
-file_path = download_data(url, output_dir)
-extracted_files = extract_data(file_path, output_dir)
-print(extracted_files)
-#write_to_gcs(bucket_name, output_dir, extracted_files)
+if __name__ == '__main__':
+    main_etl()
--- steps.md
+++ steps.md
@@ -1,1 +1,6 @@
 ## Creating a docker environment
+
+## Lunch Prefect
+
+prefect orion start
+prefect deployment build main/etl_web_gcp.py:main_etl -n "web_to_bucket"
