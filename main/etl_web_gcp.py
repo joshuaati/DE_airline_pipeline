116dacc459d3d7379385cb114ed58c854c10f586
joshuaati <joshuaati97@gmail.com>
2023-03-23 21:08:00 +0000

    Prefect development and script update

--- main/etl_web_gcp.py
+++ main/etl_web_gcp.py
@@ -1,6 +1,7 @@
 import os
 import zipfile
 import requests
+import bz2
 from typing import Tuple
 from google.cloud import storage
 from datetime import timedelta
@@ -10,13 +11,15 @@ from prefect.tasks import task_input_hash, exponential_backoff
 
 
 # os.environ['GOOGLE_APPLICATION_CREDENTIALS']='Users/x/.google/credentials/airlinepipeline.json'
-os.environ['GOOGLE_APPLICATION_CREDENTIALS']='C:/Users/x/.google/credentials/airlinepipeline.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS']='.google/airlinepipeline.json'
+
 
 # prevent timeout due to low network
 storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024* 1024  # 5 MB
 storage.blob._MAX_MULTIPART_SIZE = 5 * 1024* 1024  # 5 MB
 
-@task(name='download_data', retries=3, retry_delay_seconds=exponential_backoff(backoff_factor=10), cache_key_fn=task_input_hash, cache_expiration=timedelta(hours=2))
+
+# @task(name='download_data', retries=3, retry_delay_seconds=exponential_backoff(backoff_factor=10), cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
 def download_data(url: str, output_dir: str) -> str:
     """
     Downloads a zip file from the specified url and saves it to the specified
@@ -29,6 +32,7 @@ def download_data(url: str, output_dir: str) -> str:
     Returns:
         The path to the downloaded file.
     """
+    output_dir = os.path.abspath(output_dir)
     os.makedirs(output_dir, exist_ok=True)
     file_path = os.path.join(output_dir, os.path.basename(url))
 
@@ -38,7 +42,8 @@ def download_data(url: str, output_dir: str) -> str:
 
     return file_path
 
-@task(name='extract_data', retries=3, retry_delay_seconds=exponential_backoff(backoff_factor=5))
+
+# @task(name='extract_data', retries=3, retry_delay_seconds=exponential_backoff(backoff_factor=5))
 def extract_data(file_path: str, output_dir: str) -> Tuple[str, str]:
     """
     Extracts a zip file to the specified output directory.
@@ -50,6 +55,7 @@ def extract_data(file_path: str, output_dir: str) -> Tuple[str, str]:
     Returns:
         A tuple containing the paths to the extracted files.
     """
+    output_dir = os.path.abspath(output_dir)
     os.makedirs(output_dir, exist_ok=True)
     with zipfile.ZipFile(file_path, 'r') as zip_ref:
         zip_ref.extractall(output_dir)
@@ -61,7 +67,35 @@ def extract_data(file_path: str, output_dir: str) -> Tuple[str, str]:
 
     return tuple(extracted_files)
 
-@task(name='write_data', retries=3, retry_delay_seconds=exponential_backoff(backoff_factor=10))
+
+@task(name='extract_data', retries=3, retry_delay_seconds=exponential_backoff(backoff_factor=5))
+def extract_individual_files(extracted_files: Tuple[str, ...]) -> Tuple[str, ...]:
+    """
+    Extracts individual files from any files with a .bz2 extension in a list of extracted files.
+
+    Args:
+        extracted_files: A tuple of file paths that have been extracted from a ZIP file.
+
+    Returns:
+        A tuple of file paths that includes any files with a .bz2 extension decompressed to individual files.
+
+    """
+    new_extracted_files = []
+    for file_path in extracted_files:
+        if file_path.endswith('.bz2'):
+            with open(file_path, 'rb') as f:
+                content = f.read()
+            decompressed_content = bz2.decompress(content)
+            new_file_path = file_path[:-4]  # remove .bz2 extension
+            with open(new_file_path, 'wb') as f:
+                f.write(decompressed_content)
+            new_extracted_files.append(new_file_path)
+        else:
+            new_extracted_files.append(file_path)
+    
+    return tuple(new_extracted_files)
+
+# @task(name='write_data', retries=3, retry_delay_seconds=exponential_backoff(backoff_factor=10))
 def write_to_gcs(bucket_name: str, output_dir: str, extracted_files: Tuple[str, ...]):
     """
     Uploads the contents of each file in the extracted_files list to a Google Cloud Storage bucket.
@@ -86,7 +120,8 @@ def write_to_gcs(bucket_name: str, output_dir: str, extracted_files: Tuple[str, 
         blob = bucket.blob(filename)
         blob.upload_from_string(content)
 
-@flow()
+
+# @flow()
 def main_etl(url: str, output_dir: str, bucket_name: str) -> None:
     """
     This function is the main entry point for the ETL (Extract, Transform, Load) pipeline that downloads a zip file from a specified URL,
@@ -102,11 +137,13 @@ def main_etl(url: str, output_dir: str, bucket_name: str) -> None:
     """
     file_path = download_data(url, output_dir)
     extracted_files = extract_data(file_path, output_dir)
-    print(extracted_files)
-    write_to_gcs(bucket_name, output_dir, extracted_files)
+    individual_files = extract_individual_files(extracted_files)
+    write_to_gcs(bucket_name, output_dir, individual_files)
+
 
 if __name__ == '__main__':
     url = 'https://dataverse.harvard.edu/api/access/dataset/:persistentId/?persistentId=doi:10.7910/DVN/HG7NV7',
-    output_dir = 'data',
-    bucket_name = 'airline-buckets'
+# url = "https://github.com/DataTalsClub/nyc-tlc-data/archive/refs/tags/yellow.zip"
+    output_dir = "data"
+    bucket_name = "airline-buckets"
     main_etl(url, output_dir, bucket_name)
