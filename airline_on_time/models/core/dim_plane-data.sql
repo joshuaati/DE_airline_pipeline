03486e02888be3342ad118245ed041e85c33b03d
joshuaati <joshuaati97@gmail.com>
2023-03-30 16:20:05 +0000

    defining dimensions

--- airline_on_time/models/core/dim_plane-data.sql
+++ airline_on_time/models/core/dim_plane-data.sql
@@ -2,11 +2,12 @@ SELECT
     tailnum,
     type,
     manufacturer,
-    PARSE_DATE('%m/%d/%Y', issue_date) as issue_date,
+    CASE WHEN issue_date = 'None' THEN NULL
+    ELSE PARSE_DATE('%m/%d/%Y', issue_date) END AS issue_date,
     model,
     status,
     aircraft_type,
     engine_type,
-    CASE WHEN year = "None" THEN NULL
+    CASE WHEN year = 'None' THEN NULL
     ELSE CAST(year as INTEGER) END AS year
 FROM {{ref('plane-data')}}
