3fffc987629f849e4af96717e41780ca3ebf4fa4
joshuaati <joshuaati97@gmail.com>
2023-03-30 16:18:26 +0000

    facts table

--- airline_on_time/models/core/facts_table.sql
+++ airline_on_time/models/core/facts_table.sql
@@ -1,7 +1,26 @@
 
 WITH trip_data AS 
 (
-SELECT *
-FROM {{ref('stg_trips')}}
+    SELECT *
+    FROM {{ref('stg_trips')}}
 ),
 
+airports AS 
+(
+    SELECT * 
+    FROM {{ref{'dim_airports'}}}
+)
+
+carriers AS 
+(
+    SELECT * 
+    FROM {{ref{'dim_carriers'}}}
+)
+
+plane AS 
+(
+    SELECT * 
+    FROM {{ref{'dim_airports'}}}
+)
+
+SELECT 
